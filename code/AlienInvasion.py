259ba99eb5b9e67368a6012a63cdf6241f074236
Theis Hald Degnbol <theis8981@gmail.com>
2023-08-10 23:31:29 +0200

    ship both move left and right

--- code/AlienInvasion.py
+++ code/AlienInvasion.py
@@ -25,6 +25,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.Ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -32,6 +33,17 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.Ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.Ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.Ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.Ship.moving_left = False
 
     def _update_screen(self):
         """update image on the screen, and flip to the new screen."""
--- code/ship.py
+++ code/ship.py
@@ -25,6 +25,17 @@ class Ship:
         self.default_image_position = self.screen_rect
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blit_me(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
