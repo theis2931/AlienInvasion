52f0d4932cb696bdf6c1202fdc2407846ae745f1
Theis Hald Degnbol <theis8981@gmail.com>
2023-08-11 22:12:06 +0200

    adjust the ships speed, still scale issue

--- code/Settings.py
+++ code/Settings.py
@@ -7,5 +7,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (229, 204, 255)
-        # set the size of the image
-        self.default_image_size = (60, 60)
+        self.default_image_size = (60, 60)  # set the size of the image
+        # Ship settings
+        self.ship_speed = 0.5
--- code/ship.py
+++ code/ship.py
@@ -9,21 +9,25 @@ class Ship:
     def __init__(self, ai_game):
         """initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.Settings
         self.screen_rect = ai_game.screen.get_rect()
-        self.Settings = Settings()
 
         # load the ship image and get its rect.
         self.image = pygame.image.load('game_assets/space_ship_1.bmp')
         self.rect = self.image.get_rect()
+        # print(self.ship_image.get_rect())
 
         # scale the image to fit the screen
         # todo need to place the ship in the middle scale issue?
         self.image = pygame.transform.scale(
-            self.image, self.Settings.default_image_size)
+            self.image, self.settings.default_image_size)
 
         # Start each new ship at the bottem center of the screen.
         self.default_image_position = self.screen_rect
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
 
         # Movement flag
         self.moving_right = False
@@ -31,10 +35,15 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on the movement flag"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # todo line 39 stop before the corner scale issue
+        # update the ship's x position, not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # update rect object from self.x
+        self.rect.x = self.x
 
     def blit_me(self):
         """Draw the ship at its current location."""
